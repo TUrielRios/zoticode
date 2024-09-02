d0042572ccab3a813fe2cffe09bb8f74275ca36c
unknown <riostiziano6@gmail.com>
2024-09-02 12:53:20 -0300

    Publicado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import  Header  from './Components/Header/Header';
+import VideoList from './Components/VideoList/VideoList';
+import ProjectGallery from './Components/ProjectGallery/ProjectGallery';
+import MessageBoard from './Components/MessageBoard/MessageBoard';
+import  HeroSection  from './Components/HeroSection/HeroSection';
+import { CoursesSlider } from './Components/CoursesSlider/CoursesSlider';
+import FeaturesSection from './Components/FeaturesSection/FeaturesSection';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import PopularCourses from './Components/PopularCourses/PopularCourses';
+import CallToActionSection from './Components/CallToActionSection/CallToActionSection';
+import TestimonialsSection from './Components/TestimonialsSection/TestimonialsSection'
+import LearningSection from './Components/LearningSection/LearningSection';
+import ProgrammingSection from './Components/ProgrammingSection/ProgrammingSection';
+import './App.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main>
+        <HeroSection />
+        <ProgrammingSection />
+        <FeaturesSection />
+        <PopularCourses/>
+        <TestimonialsSection />
+        <CallToActionSection />
+        
+      </main>
     </div>
   );
 }

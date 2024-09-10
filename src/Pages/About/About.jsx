f5d42db8624be694c9df9b3b0372e1a46bed392a
unknown <riostiziano6@gmail.com>
2024-09-10 18:42:07 -0300

    codeeditor bye

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,11 +1,10 @@
-import CodeEditor from "../../Components/CodeEditor/CodeEditor";
+
 import styles from './About.module.css'
 
 const About = () => {
     return(
         <div className={styles.container}>
             <h1>soy el about</h1>
-            <CodeEditor />
         </div>
     )
 }
